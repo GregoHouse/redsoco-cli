db8fa9a18e3a3d217db572563fb7267173dc365e
Maria Nazareth Garcia Masjoan <maria@Marias-MacBook-Pro.local>
2024-07-22 23:18:14 -0300

    first commit.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+};
